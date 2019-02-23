bfb9141d9c533f13af2e3bd0fe757ca9187ccaed
yonashabte <yonaschereta@gmail.com>
2019-02-23 15:29:30 -0500

    week1

---
+++ main.js
@@ -0,0 +1,16 @@
+'use strict'
+//let pageone = document.getElementById('paragtwo')
+let question1 = prompt('Is Yonas myname?')
+let question2 = prompt('is Hidden Figures my favorite movie?')
+let question3 = prompt('Am I a teacher')
+
+if (question1 === 'Yonas') {
+
+    console.log('Yes')
+
+    if (question2 === 'Hidden Figures')
+    console.log('Yes')
+ }
+    else {
+        console.log('No')
+    }
